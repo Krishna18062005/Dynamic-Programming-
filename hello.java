a85c3657182f998c08bcb2625cc742bd7c4f13fd
Krishna K <136406756+Krishna18062005@users.noreply.github.com>
2024-05-22 07:18:21 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -6,32 +6,44 @@ public class Hello {
 		int N=sc.nextInt();
 		if(N<=1){ System.out.print("YES");
 		return;}
-		boolean rslt=true;
-		char fir=97;
+		boolean rslt;
 		char[][] matri=new char[N][N];
-		char sec=97;
 		for(int i=0;i<N;i++){
 		for(int j=0;j<N;j++){
-		    char curr=sc.next().charAt(0);
-		    matri[i][j]=curr;
-		    
-		    if(i==0&&j==0) fir=curr;
-		    if(i==0&&j==1) sec=curr;
-		    else if(fir!=curr&&(i==j)) rslt=false;
-		    else if(fir!=curr&&(N==i+j+1)) rslt=false;
-		    
+		    matri[i][j]=sc.next().charAt(0);
 		}}
-		System.out.print((checkNonDiag(matri)&&rslt)?"YES":"NO");
+		rslt=get(matri,N);
+		System.out.print((rslt)?"YES":"NO");
 	}
-	public static boolean checkNonDiag(char[][] matri){
-	    char a=matri[0][1];
-	    for(int i=0;i<N;i++){
-	        for(int j=0;j<N;j++){
-	            if(i!=j&&i!=j-N+1&&a!=matri[i][j]){
+	public static boolean get(char[][] ma,int n){
+	    char dia =ma[0][0];
+	    char nondia=ma[0][1];
+	   
+	    for(int i=0;i<n;i++)
+	    {
+	        for(int j=0;j<n;j++)
+	        {
+	          if(i==j||n==j+i+1)
+	          {
+	           if(ma[i][j]!=dia) 
+	            {
+	                
+	            return false;
+	               }
+	             }
+	             
+	        
+	               
+	              
+	          else{
+	            if((ma[i][j]!=(nondia))&&i!=j&&i!=j-n+1)
+	            
+	            { 
 	                return false;
 	            }
-	        }
-	    }
+	              }
+	         }}
+	    
 	    return true;
 	}
 }
